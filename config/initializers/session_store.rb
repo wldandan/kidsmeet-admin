9e0a1214af7e231e5cb5583aa8a92b012ae2952f
Wang Lei <wldandan@gmail.com>
2014-10-10 16:18:48 +0800

    update the session key to 'kidsmeets'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_app-admin_session'
+Rails.application.config.session_store :cookie_store, key: '_kidsmeets-admin_session'
