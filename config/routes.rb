9d01ae9f3a3c1b4b04da18a6f50e222872b776ce
Wang Lei <wldandan@gmail.com>
2014-11-30 15:27:55 +0800

    change the name to '咖萌亲子游'

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Ckeditor::Engine => '/ckeditor'
 
-
+  root :to => redirect('/admin')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
