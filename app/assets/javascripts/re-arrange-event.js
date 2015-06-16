56f88158b5c8c8b995baf3f29ae02f442bc66302
Guoqing <zhao.guo@rea-group.com>
2015-06-16 22:08:17 +0800

    animation for sign up button on mobile

--- app/assets/javascripts/re-arrange-event.js
+++ app/assets/javascripts/re-arrange-event.js
@@ -1,14 +1,30 @@
 $(function(){
     if( $(window).width() <= 767) {
-      $('.activity-intro .intro').insertAfter($('#sign-up-from'));
+      $('#sign-up-from').insertAfter($('.default-content'));
+
+      $('.sign-up-tab a').click(displaySignUpContainer);
+      $('.close-icon').click(removeSignUpContainer);
     }
   }
 );
 
 window.onresize = function() {
   if( $(window).width() <= 767) {
-    $('.activity-intro .intro').insertAfter($('#sign-up-from'));
+    $('#sign-up-from').insertAfter($('.default-content'));
+
+    $('.sign-up-tab a').click(displaySignUpContainer);
+    $('.close-icon').click(removeSignUpContainer);
   } else {
-    $('.right-box .intro').insertAfter($('.gallery'));
+    $('#sign-up-from').insertAfter($('.right-box .event-detail'));
   }
 };
+
+function displaySignUpContainer() {
+  $('.cover').show();
+  $('.sign-up-container').css({ "display": "block", top: $(window).height() }).animate({ top: 409 }, 300);
+}
+
+function removeSignUpContainer() {
+  $('.cover').hide();
+  $('.sign-up-container').css({ "display": "none", top: 409 }).animate({ top: $(window).height() }, 300);
+}
