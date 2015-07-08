6785f2c82e3d77a43140107bc6f627bdd8ddf910
Guoqing <zhao.guo@rea-group.com>
2015-07-08 21:54:01 +0800

    Add animation for masonry

--- app/assets/javascripts/masonry.js
+++ app/assets/javascripts/masonry.js
@@ -1,5 +1,11 @@
 window.onload = function() {
   $('.masonry-container').masonry({
-    itemSelector : '.graph-container'
+    itemSelector : '.graph-container',
+    isAnimated: true,
+    animationOptions: {
+      duration: 500,
+      easing: 'linear',
+      queue: false
+    }
   });
 }
